443c8eae1eecd122b0249bd225fc9dc74fc24614
MuGaber <mohammedgaber273@gmail.com>
2020-03-17 23:39:22 +0200

    added side nav bar

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+//
 const toggleMenuIcon = el => {
   el.classList.toggle('change-icon')
   document.getElementById('nav-bar').classList.toggle('hide')
 }
+
+//
+const openTab = (event, tabName) => {
+  let tabcontent, tablinks
+
+  tabcontent = document.getElementsByClassName('tabcontent')
+  for (tab of tabcontent) tab.style.display = 'none'
+
+  tablinks = document.getElementsByClassName('tablinks')
+  for (link of tablinks) link.classList.remove('active')
+
+  document.getElementById(tabName).style.display = 'block'
+  event.currentTarget.classList.add('active')
+}
+
+//
+const main = () => {
+  document.getElementById('default-tab').click()
+}
+
+main()
